8191f1d9ffcfde37080454c490db1650aa1e053f
majian <majian159@live.com>
2015-10-28 16:36:40 +0800

    添加分布式工具类库（分布式锁）。

---
+++ Distributed/Distributed.Utility/Impl/ClrDistributedLock.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace Distributed.Utility.Impl
+{
+    /// <summary>
+    /// 基于Monitor的分布式锁。
+    /// </summary>
+    public sealed class ClrDistributedLock : IDistributedLock
+    {
+        private readonly ConcurrentDictionary<string, object> _dictionary = new ConcurrentDictionary<string, object>();
+
+        #region Implementation of IDistributedLock
+
+        /// <summary>
+        /// 锁定（超时时间为30秒）。
+        /// </summary>
+        /// <param name="token">锁记号。</param>
+        public void Lock(string token)
+        {
+            TryLock(token, TimeSpan.FromSeconds(10));
+        }
+
+        /// <summary>
+        /// 尝试锁定。
+        /// </summary>
+        /// <param name="token">锁记号。</param>
+        /// <param name="timeout">超时时间。</param>
+        public void TryLock(string token, TimeSpan timeout)
+        {
+            var syncLock = _dictionary.GetOrAdd(token, key => new object());
+            Monitor.TryEnter(syncLock, timeout);
+        }
+
+        /// <summary>
+        /// 解锁。
+        /// </summary>
+        /// <param name="token">锁记号。</param>
+        public void UnLock(string token)
+        {
+            object syncLock;
+            if (_dictionary.TryRemove(token, out syncLock))
+                Monitor.Exit(syncLock);
+        }
+
+        #endregion Implementation of IDistributedLock
+    }
+}
